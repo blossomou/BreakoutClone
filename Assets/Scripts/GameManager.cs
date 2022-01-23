17f2967bac707ff141f7879d6812230f3df2f4a7
blossomou <qingqingou@gmail.com>
2022-01-23 17:34:00 -0500

    Created GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text ballsText;
+    [SerializeField] private Text levelText;
+
+    [SerializeField] private GameObject panelMenu;
+    [SerializeField] private GameObject panelPlay;
+    [SerializeField] private GameObject panelLevelCompleted;
+    [SerializeField] private GameObject panelGameOver;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
