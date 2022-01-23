cc3cf6d46be6afec4a17de57f3db4a32d755c05f
blossomou <qingqingou@gmail.com>
2022-01-23 00:15:01 -0500

    Add paddle player

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private float speed = 20f; //20 units per second
-    private Rigidbody rigidbody;
+    private Rigidbody _rigidbody;
     private Vector3 velocity;
 
     
@@ -13,8 +13,8 @@ public class Ball : MonoBehaviour
     void Start()
     {
         
-        rigidbody = GetComponent<Rigidbody>();
-        rigidbody.velocity = Vector3.down * speed;
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.velocity = Vector3.down * speed;
     }
 
     // Update is called once per frame
@@ -25,10 +25,10 @@ public class Ball : MonoBehaviour
 
     private void FixedUpdate() {
         
-        rigidbody.velocity = rigidbody.velocity.normalized * speed; //whatever the speed the ball is moving, it will take the value to 1 using normalized
-        velocity = rigidbody.velocity;
+        _rigidbody.velocity = _rigidbody.velocity.normalized * speed; //whatever the speed the ball is moving, it will take the value to 1 using normalized
+        velocity = _rigidbody.velocity;
     }
      private void OnCollisionEnter(Collision collision) {
-        rigidbody.velocity = Vector3.Reflect(velocity, collision.contacts[0].normal); //normal is the first point in the collision of perpendicular impact of itself
+        _rigidbody.velocity = Vector3.Reflect(velocity, collision.contacts[0].normal); //normal is the first point in the collision of perpendicular impact of itself
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        _rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        _rigidbody.MovePosition(new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, 50)).x, -17, 0));
+    }
+}
